6fdd808142e1225ab24ba4de874bcdf0d365f6f2
Martín <magadiflo@gmail.com>
2022-02-04 17:21:06 -0500

    Anotación @RequestMapping sobre el controlador

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -3,11 +3,13 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping({"/", "/index", "/home"}) //Un método puede estar mapeado a más de una ruta URL
+	@GetMapping({"", "/", "/index", "/home"}) //Un método puede estar mapeado a más de una ruta URL
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hello Spring Framework From Controller");
 		return "index";//index.html
