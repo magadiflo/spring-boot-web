60dd955b089df196237a739417ea701ea5192f9e
Martín <magadiflo@gmail.com>
2022-02-07 17:44:06 -0500

    Inyectar valores usando la anotación @Value

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,19 @@ import com.bolsadeideas.springboot.web.app.models.Usuario;
 @Controller
 @RequestMapping("/app")
 public class IndexController {
+	
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
 
 	@GetMapping({ "", "/", "/index", "/home" }) // Un método puede estar mapeado a más de una ruta URL
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hello Spring Framework From Controller");
+		model.addAttribute("titulo", this.textoIndex);
 		return "index";// index.html
 	}
 
@@ -29,14 +39,14 @@ public class IndexController {
 		usuario.setEmail("magadiflo@gmail.com");
 
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil de usuario: ".concat(usuario.getNombre()));
+		model.addAttribute("titulo", this.textoPerfil.concat(usuario.getNombre()));
 
 		return "perfil";
 	}
 
 	@RequestMapping("/listar")
 	public String listar(Model model) {
-		model.addAttribute("titulo", "Listado de usuarios");
+		model.addAttribute("titulo", this.textoListar);
 
 		return "listar";
 	}
